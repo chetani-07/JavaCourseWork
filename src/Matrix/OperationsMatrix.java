4f52f85176f6ccbbaca4a68c16cbf5b0b9ada85a
chetani-07 <chetaningole0007@gmail.com>
2023-12-25 01:13:11 +0530

    Matrix Package Added.

---
+++ src/Matrix/OperationsMatrix.java
@@ -0,0 +1,56 @@
+package Matrix;
+
+public class OperationsMatrix {
+
+    //Method to Print Matrix.
+    public static void printMatrix(int matrix[][], int m, int n) {
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print(matrix[row][col] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------");
+    }
+
+    //Method to Add Two Given Matrices.
+    public static int[][] addMatrices(int[][] mat1, int[][] mat2, int m, int n) {
+        int[][] res = new int[m][n];
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                res[row][col] = mat1[row][col] + mat2[row][col];
+            }
+        }
+        return res;
+    }
+
+    //Method to perform subtraction among matrices.
+    public static int[][] subtractMatrices(int[][] mat1, int[][] mat2, int m, int n) {
+        int[][] res = new int[m][n];
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                res[row][col] = mat1[row][col] - mat2[row][col];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat1 = {{3, 2, 5}, {2, 7, 5}, {4, 6, 1}};
+        int[][] mat2 = {{1, 6, 2}, {4, 1, 2}, {2, 4, 1}};
+
+        System.out.println("Matrix 1: ");
+        printMatrix(mat1, 3, 3);
+        System.out.println("Matrix 2: ");
+        printMatrix(mat2, 3, 3);
+
+        int[][] sum=addMatrices(mat1,mat2,3,3);
+        System.out.println("Addition of Matrices: ");
+        printMatrix(sum,3,3);
+
+        int[][] sub=subtractMatrices(mat1,mat2,3,3);
+        System.out.println("Subtraction of Matrices: ");
+        printMatrix(sub,3,3);
+
+    }
+}
